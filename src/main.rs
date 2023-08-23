8fc4aa1c68432cef5b883ec8f5471c1a3614ee0a
Rahul <basicfunc@gmail.com>
2023-08-23 16:20:14 +0530

    Proper error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![feature(seek_stream_len)]
 use std::{
+    eprintln,
     fs::File,
     io::{Read, Seek, Write},
     path::PathBuf,
@@ -8,6 +9,7 @@ use std::{
 
 use argh::FromArgs;
 use bus::Bus;
+use thiserror::Error;
 
 #[derive(FromArgs, Debug)]
 /// Simple interface to write image to many files/devices at once, can also be used to backup to multiple locations
@@ -34,25 +36,27 @@ struct Diode {
 }
 
 impl Diode {
-    fn run(self) -> Result<Status, Box<std::io::Error>> {
+    fn run(self) -> Result<Status, Error> {
         let mut message_bus: Bus<Vec<u8>> = Bus::new(self.block_buffer);
         let input_path = self.input.clone();
         let outputs = self.output.clone();
 
-        let writer_threads: Vec<thread::JoinHandle<Result<(), std::io::Error>>> = outputs
+        let writer_threads: Vec<thread::JoinHandle<Result<(), Error>>> = outputs
             .into_iter()
             .map(|output_path| {
                 let mut recv = message_bus.add_rx();
                 thread::spawn(move || {
-                    let mut file = File::create(&output_path)?;
+                    let mut file = File::create(&output_path)
+                        .map_err(|_| Error::UnableToCreateFile(output_path))?;
 
                     loop {
                         match recv.recv() {
                             Ok(bytes) => {
-                                file.write_all(&bytes)?;
+                                file.write_all(&bytes)
+                                    .map_err(|_| Error::UnableToWriteToBuffer)?;
                             }
-                            Err(_err) => {
-                                file.sync_all()?;
+                            Err(_) => {
+                                file.sync_all().map_err(|_| Error::UnableToSyncFiles)?;
                                 break;
                             }
                         }
@@ -63,43 +67,63 @@ impl Diode {
             })
             .collect();
 
-        let reader_thread: thread::JoinHandle<Result<usize, std::io::Error>> =
-            thread::spawn(move || {
-                let mut file = File::open(input_path)?;
+        let reader_thread: thread::JoinHandle<Result<usize, Error>> = thread::spawn(move || {
+            let mut file =
+                File::open(&input_path).map_err(|_| Error::UnableToOpenFile(input_path.clone()))?;
 
-                let mut read = 0;
+            let mut read = 0;
 
-                match self.block_count {
-                    Some(count) => {
-                        let mut counter = 0;
-                        while counter < count {
-                            let mut tmp_buf = vec![0; self.block_size];
-                            read += file.read(&mut tmp_buf)?;
-                            message_bus.broadcast(tmp_buf);
+            match self.block_count {
+                Some(count) => {
+                    let mut counter = 0;
+                    while counter < count {
+                        let mut tmp_buf = vec![0; self.block_size];
+                        read += file
+                            .read(&mut tmp_buf)
+                            .map_err(|_| Error::UnableToReadBytesFrom(input_path.clone()))?;
+                        message_bus.broadcast(tmp_buf);
 
-                            counter += 1;
-                        }
+                        counter += 1;
                     }
-                    None => {
-                        while file.stream_position()? < file.stream_len()? {
-                            let diff = (file.stream_len()? - file.stream_position()?) as usize;
+                }
+                None => {
+                    let input = input_path.clone();
+                    loop {
+                        let curr_pos = file
+                            .stream_position()
+                            .map_err(|_| Error::UnableToGetCurrPos(input.clone()))?;
+
+                        let full_len = file
+                            .stream_len()
+                            .map_err(|_| Error::UnableToGetByteLen(input.clone()))?;
+
+                        if curr_pos < full_len {
+                            let diff = (full_len - curr_pos) as usize;
 
                             let mut tmp_buf = if diff < self.block_size {
                                 vec![0; diff]
                             } else {
                                 vec![0; self.block_size]
                             };
-                            read += file.read(&mut tmp_buf)?;
+
+                            read += file
+                                .read(&mut tmp_buf)
+                                .map_err(|_| Error::UnableToReadBytesFrom(input.clone()))?;
                             message_bus.broadcast(tmp_buf);
+                        } else {
+                            break;
                         }
                     }
-                };
+                }
+            };
 
-                Ok(read)
-            });
+            Ok(read)
+        });
 
         // Wait on threads
-        let bytes_read = reader_thread.join().unwrap()?;
+        let bytes_read = reader_thread
+            .join()
+            .map_err(|_| Error::FailedToJoinThreads)??;
 
         let st = Status {
             bytes_copied: bytes_read,
@@ -107,7 +131,7 @@ impl Diode {
         };
 
         for handle in writer_threads {
-            handle.join().unwrap().unwrap();
+            handle.join().map_err(|_| Error::FailedToJoinThreads)??;
         }
 
         Ok(st)
@@ -117,6 +141,26 @@ impl Diode {
 struct Status {
     bytes_copied: usize,
     num_of_files: usize,
+}
+
+#[derive(Error, Debug)]
+enum Error {
+    #[error("Error ocuured while writing to buffer.")]
+    UnableToWriteToBuffer,
+    #[error("Error ocuured while syncing all files.")]
+    UnableToSyncFiles,
+    #[error("Error ocuured while creating file: {0}.")]
+    UnableToCreateFile(PathBuf),
+    #[error("Error ocuured while opening file: {0}.")]
+    UnableToOpenFile(PathBuf),
+    #[error("Error ocuured while reading bytes from file: {0}.")]
+    UnableToReadBytesFrom(PathBuf),
+    #[error("Error ocuured while getting current seek position of file: {0}.")]
+    UnableToGetCurrPos(PathBuf),
+    #[error("Error ocuured while getting last seek position of file: {0}.")]
+    UnableToGetByteLen(PathBuf),
+    #[error("Error ocuured while waiting for threads.")]
+    FailedToJoinThreads,
 }
 
 impl std::fmt::Display for Status {
@@ -130,7 +174,8 @@ impl std::fmt::Display for Status {
 }
 
 fn main() {
-    let args: Diode = argh::from_env();
-    let runner = args.run().unwrap();
-    println!("{runner}");
+    match argh::from_env::<Diode>().run() {
+        Ok(st) => println!("{st}"),
+        Err(err) => eprintln!("{err}"),
+    }
 }
